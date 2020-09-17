05cd7ed86e6e37b9b4f4eb7836d3558d515c3bf5
Tina-Lightning <c.atherholt@gmail.com>
2020-09-17 11:03:14 -0400

    created some styles with tailwind css

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const API_URL = "https://opentdb.com/api.php?amount=10&category=14&type=multiple";
 
 function App() {
+
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    fetch(API_URL)
+      .then((res) => res.json())
+      .then((data) => {
+        // this puts the results inside the questions array
+        setQuestions(data.results);
+      });
+  }, []);
+
   return (
-    <div className="container">
-      <div>
-        Question
+      questions.length > 0 ? (
+        <div className="container">
+        <div className="bg-white text-purple-800 p-10 rounded-lg shadow-md">
+        <h2 className="text-2xl">
+        {questions[0].question}
+        </h2>
       </div>
-      <div>
-        <div>Answer</div>
-        <div>Answer</div>
-        <div>Answer</div>
-        <div>Answer</div>
+      <div className="grid grid-cols-2 gap-6 mt-6">
+        <button className="bg-white p-4 text-purple-800 font-semibold rounded shadow">{questions[0].correct_answer}</button>
+        <button className="bg-white p-4 text-purple-800 font-semibold rounded shadow">Answer</button>
+        <button className="bg-white p-4 text-purple-800 font-semibold rounded shadow">Answer</button>
+        <button className="bg-white p-4 text-purple-800 font-semibold rounded shadow">Answer</button>
       </div>
-    </div>
+      </div>
+      ) : ( <h1>No questions sorry</h1> )
   );
 }
 
