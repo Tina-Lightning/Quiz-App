264bc73ef7e5902037ec036efb5429017b3bb393
Tina-Lightning <c.atherholt@gmail.com>
2020-09-24 10:00:49 -0400

    quiz app is finished

--- src/App.js
+++ src/App.js
@@ -9,41 +9,58 @@ function App() {
   const [questions, setQuestions] = useState([]);
   const [currentIndex, setCurrentIndex] = useState(0);
   const [score, setScore] = useState(0);
-  const [gameEnded, setGameEnded] = useState(false);
+  const [showAnswers, setShowAnswers] = useState(false);
 
   useEffect(() => {
     fetch(API_URL)
       .then((res) => res.json())
       .then((data) => {
         // this puts the results inside the questions array
-        setQuestions(data.results);
-        setCurrentIndex(0);
+        //setQuestions(data.results);
+        //setCurrentIndex(0);
+        const questions = data.results.map((question) => 
+        ({
+          ...question,
+          answers: [
+            question.correct_answer,
+            ...question.incorrect_answers
+          ].sort(() => Math.random() - 0.5)
+        }))
+        setQuestions(questions);
       });
   }, []);
 
   const handleAnswer = (answer) => {
-    const newIndex = currentIndex + 1
-  
-    setCurrentIndex(newIndex);
-
-    if(answer === questions[currentIndex].correct_answer) {
-      setScore(score + 1);
-    }
-
-    if(newIndex >= questions.length) {
-      setGameEnded(true);
+    if (!showAnswers) {
+      // prevent double answers
+      if (answer === questions[currentIndex].correct_answer) {
+        setScore(score + 1);
+      }
+      setShowAnswers(true);
     }
   }
 
-  return gameEnded ? (
-  <h1 className="text-3xl text-white font-bold">Your score was {score}</h1>
+  const handleNextQuestion = () => {
+    setShowAnswers(false);
+    setCurrentIndex(currentIndex + 1);
+  }
+
+  return questions.length > 0 ? (
+    <div className="container">
+      {currentIndex >= questions.length ? (
+        <h1 className="text-3xl text-white font-bold">Game Over! Your score is: {score}</h1>
+      ) : (
+          <QuestionBox
+            data={questions[currentIndex]}
+            showAnswers={showAnswers}
+            handleNextQuestion={handleNextQuestion}
+            handleAnswer={handleAnswer}
+          />
+        )}
+    </div>
   ) : (
-      questions.length > 0 ? ( 
-        <div className="container">
-          <QuestionBox data={questions[currentIndex]} handleAnswer={handleAnswer} /> 
-        </div>
-      ) : ( <h2 className="text-2xl text-white font-bold">Loading...</h2> )
-  );
+      <h2 className="text-2xl text-white font-bold">Loading...</h2>
+    );
 }
 
 export default App;
--- src/components/QuestionBox.js
+++ src/components/QuestionBox.js
@@ -1,23 +1,40 @@
 import React from "react";
 
-const QuestionBox = ({ handleAnswer, data: { question, correct_answer, incorrect_answers }, }) => {
-
-    const randomAnswers = [ correct_answer, ...incorrect_answers].sort(() => Math.random() - 0.5);
+const QuestionBox = ({
+    showAnswers,
+    handleAnswer,
+    handleNextQuestion,
+    data: { question, correct_answer, answers },
+}) => {
 
     return (
-    <div>
-        <div className="bg-white text-purple-800 p-10 rounded shadow-md">
-            <h2 className="text-2xl" dangerouslySetInnerHTML={{ __html: question }} />
+        <div className="flex flex-col">
+            <div className="bg-white text-purple-800 p-10 rounded shadow-md">
+                <h2 className="text-2xl" dangerouslySetInnerHTML={{ __html: question }} />
+            </div>
+            <div className="grid grid-cols-2 gap-6 mt-6">
+                {answers.map((answer, index) => {
+
+                    const textColor = showAnswers ? answer === correct_answer ? "text-green-500" : "text-red-500" : "text-purple-800";
+
+                    //const textColor = showAnswers ? "text-white" : "text-purple-800";
+
+                    return (
+                        <button key={index} className={`${textColor} bg-white p-4 font-semibold rounded shadow`}
+                            onClick={() => handleAnswer(answer)}
+                            dangerouslySetInnerHTML={{ __html: answer }}
+                        />
+                    )
+                })}
+            </div>
+            {showAnswers && (
+                <button 
+                onClick={handleNextQuestion}
+                className="bg-purple-700 text-white p-4 font-semibold rounded shadow mt-6">Next Question</button>
+            )}
+            
         </div>
-        <div className="grid grid-cols-2 gap-6 mt-6">
-            {randomAnswers.map(answer => (
-                <button className={`${correct_answer === answer ? "bg-purple-300" : "bg-white"} p-4 text-purple-800 font-semibold rounded shadow`}
-                onClick={() => handleAnswer(answer)}>
-                    {answer}
-                </button>
-            ))}
-        </div>
-    </div>
-)};
+    )
+};
 
 export default QuestionBox;
